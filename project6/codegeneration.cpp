8c5c3816616f8f09aba07cd18bf3cc0cd663bfb3
Katyayani Raman <katyayani@csilvm-04.cs.ucsb.edu>
2022-06-02 14:50:57 -0700

    added code plus old code

--- project6/codegeneration.cpp
+++ project6/codegeneration.cpp
@@ -6,22 +6,32 @@
 
 void CodeGenerator::visitProgramNode(ProgramNode* node) {
     // WRITEME: Replace with code if necessary
+  std::cout << "# PROGRAM NODE" << std::endl;
+  std::cout << " .data" << std::endl; //check
+  std::cout << "printstr: .asciz \"%d\\n\"" << std::endl << std::endl;
+  std::cout << " .text" << std::endl;
+  std::cout << " .globl Main_main:" << std::endl;
+  node->visit_children(this);
 }
 
 void CodeGenerator::visitClassNode(ClassNode* node) {
     // WRITEME: Replace with code if necessary
+  node->visit_children(this);
 }
 
 void CodeGenerator::visitMethodNode(MethodNode* node) {
     // WRITEME: Replace with code if necessary
+  node->visit_children(this);
 }
 
 void CodeGenerator::visitMethodBodyNode(MethodBodyNode* node) {
     // WRITEME: Replace with code if necessary
+  node->visit_children(this);
 }
 
 void CodeGenerator::visitParameterNode(ParameterNode* node) {
     // WRITEME: Replace with code if necessary
+  node->visit_children(this);
 }
 
 void CodeGenerator::visitDeclarationNode(DeclarationNode* node) {
@@ -29,121 +39,253 @@ void CodeGenerator::visitDeclarationNode(DeclarationNode* node) {
 }
 
 void CodeGenerator::visitReturnStatementNode(ReturnStatementNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
 }
 
-void CodeGenerator::visitAssignmentNode(AssignmentNode* node) {
-    // WRITEME: Replace with code if necessary
+void CodeGenerator::visitAssignmentNode(AssignmentNode* node) { //could be wrong
+  // WRITEME: Replace with code if necessary
+  // std::cout << "# ASSIGNMENT" << std::endl;
+  // node->visit_children(this);
+  // std::cout << "  pop %eax" << std::endl;
+  // std::cout <<  "  mov %eax, " << offset << "(%ebp)" << std::endl;
 }
 
 void CodeGenerator::visitCallNode(CallNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
 }
 
-void CodeGenerator::visitIfElseNode(IfElseNode* node) {
-    // WRITEME: Replace with code if necessary
+void CodeGenerator::visitIfElseNode(IfElseNode* node) { //could be wrong
+  // WRITEME: Replace with code if necessary
+  std::cout << "# IF ELSE" << std::endl;
+  node->accept(this);
+
+  std::string ifLabel = "label_" + std::to_string(nextLabel());
+  std::cout << "  pop %ebx" << std::endl; //reg2 (check)
+  std::cout << "  pop %eax" << std::endl; //reg1 (check)
+   
+  std::cout << "  cmp %eax, %ebx" << std::endl;
+  std::cout << "  je " << ifLabel << std::endl;
+  node->visit_children(this); //is this how we do it
+  //how to not do this if if loop is taken? 
+  std::string elseLabel = "label_" + std::to_string(nextLabel());
+  std::cout << "  jne " << elseLabel << std::endl;
+  node->visit_children(this); //wub>
+   
 }
 
-void CodeGenerator::visitWhileNode(WhileNode* node) {
-    // WRITEME: Replace with code if necessary
+void CodeGenerator::visitWhileNode(WhileNode* node) { //might be wrong
+  //WRITEME: Replace with code if necessary
+  std::cout << "# WHILE" << std::endl;
+  node->accept(this);
+  std::string whileLabel = "label_" + std::to_string(nextLabel()); 
+  std::cout << "  pop %ebx" << std::endl; //reg2 (check)
+  std::cout << "  pop %eax" << std::endl; //reg1 (check)
+   
+  std::cout << "  cmp %eax, %ebx" << std::endl;
+  std::cout << "  je " << whileLabel << std::endl;
+  std::string after_whileLabel = "label_" + std::to_string(nextLabel());
+  std::cout << "  jne " << after_whileLabel << std::endl; 
 }
 
 void CodeGenerator::visitPrintNode(PrintNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
+  std::cout << "# PRINT" << std::endl;
+  std::cout << " push $printstr" << std::endl;
+  std::cout << " call printf" << std::endl;
+  //std::cout << " pop $printstr" << std::endl; //clean stack
+  
 }
 
 void CodeGenerator::visitDoWhileNode(DoWhileNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
+  std::cout << "# DO WHILE" << std::endl;
 }
 
 void CodeGenerator::visitPlusNode(PlusNode* node) {
-    // WRITEME: Replace with code if necessary
+  node->visit_children(this);
+  std::cout << "  # PLUS" << std::endl;
+  std::cout << "  pop %edx" << std::endl; //reg2
+  std::cout << "  pop %eax" << std::endl; //reg1
+  std::cout << "  add %edx, %eax" << std::endl; //(reg1 + reg2) -> reg1
+  std::cout << "  push %eax" << std::endl; //push reg1
+  // WRITEME: Replace with code if necessary
 }
 
 void CodeGenerator::visitMinusNode(MinusNode* node) {
-    // WRITEME: Replace with code if necessary
+  node->visit_children(this);
+  std::cout << "  # MINUS" << std::endl;
+  std::cout << "  pop %edx" << std::endl; //reg2
+  std::cout << "  pop %eax" << std::endl; //reg1
+  std::cout << "  sub %eax, %edx" << std::endl; //(reg2 - reg1) -> reg2
+  std::cout << "  push %edx" << std::endl; //push reg2
+  // WRITEME: Replace with code if necessary
 }
 
 void CodeGenerator::visitTimesNode(TimesNode* node) {
-    // WRITEME: Replace with code if necessary
+  node->visit_children(this);
+  std::cout << "  # MULTIPLY" << std::endl;
+  std::cout << "  pop %edx" << std::endl; //reg2
+  std::cout << "  pop %eax" << std::endl; //reg1
+  std::cout << "  imul %edx, %eax" << std::endl; //(reg1 * reg2) -> reg1
+  std::cout << "  push %eax" << std::endl; //push reg1
+  // WRITEME: Replace with code if necessary
 }
 
 void CodeGenerator::visitDivideNode(DivideNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
+  node->visit_children(this);
+  std::cout << "  # DIVIDE" << std::endl;
+  std::cout << "  pop %ebx" << std::endl; //reg2 = denominator 
+  std::cout << "  pop %eax" << std::endl; //reg1 = numerator
+  std::cout << " cdq" << std::endl; //sign extending eax into edx for numerator
+  std::cout << "  idiv %ebx" << std::endl; //(%edx : %eax) / %ebx -> %eax
+  std::cout << "  push %eax" << std::endl; //quotient result of division
+}
+//check
+void CodeGenerator::visitGreaterNode(GreaterNode* node) { //jump instr using cmp and jg
+  // WRITEME: Replace with code if necessary
+  node->visit_children(this);
+  std::cout << " #GREATER_THAN" << std::endl; 
+  std::string greaterLabel = "label_" + std::to_string(nextLabel());
+  std::cout << "  pop %ebx" << std::endl; //reg2
+  std::cout << "  pop %eax" << std::endl; //reg1  
+  std::cout << "  cmp %ebx, %eax" << std::endl;
+  std::cout << " setg %al" << std::endl;
+  std::cout << " movzx %al, %eax" << std::endl;
+  std::cout << " push %eax" << std::endl; 
+
+  std::cout << "  jg " << greaterLabel << std::endl;
+  std::string label = "label_" + std::to_string(nextLabel());
+  std::cout << " jmp  " << label << std::endl;
+  
+}
+//check
+void CodeGenerator::visitGreaterEqualNode(GreaterEqualNode* node) { //jump instr using cmp and jge
+  // WRITEME: Replace with code if necessary
+  node->visit_children(this);
+  std::cout << " #GREATER_THAN_OR_EQUAL" << std::endl; 
+  std::string greater_equalLabel = "label_" + std::to_string(nextLabel());
+  std::cout << "  pop %ebx" << std::endl; //reg2
+  std::cout << "  pop %eax" << std::endl; //reg1  
+  std::cout << "  cmp %ebx, %eax" << std::endl;
+  std::cout << " setge %al" << std::endl;
+  std::cout << " movzx %al, %eax" << std::endl;
+  std::cout << " push %eax" << std::endl; 
+
+  std::cout << "  jge " << greater_equalLabel << std::endl;
+  std::string label = "label_" + std::to_string(nextLabel());
+  std::cout << " jmp  " << label << std::endl;
+    
+  //might be wrong
+}
+//check
+void CodeGenerator::visitEqualNode(EqualNode* node) { //jump instr using cmp and je
+  // WRITEME: Replace with code if necessar
+  node->visit_children(this);
+   std::cout << "  # Equal Node" << std::endl;
+   std::string equalLabel = "label_" + std::to_string(nextLabel());   
+    std::cout << " pop %ebx" << std::endl;
+    std::cout << " pop %eax" << std::endl;
+    std::cout << " cmp %ebx, %eax" << std::endl;
+    std::cout << " je " << equalLabel << std::endl;
+    std::cout << " push $1" << std::endl;
+    std::string not_equalLabel = "label_" + std::to_string(nextLabel());   
+    std::cout << " jmp  " << not_equalLabel << std::endl;
+    //std::cout << lab1 << ": " << std::endl;
+    std::cout << " push $0" << std::endl;
+    //std::cout << lab2 << ": " << std::endl;
+    
+  // std::cout << " #EQUAL" << std::endl; 
+  // std::string equalLabel = "label_" + std::to_string(nextLabel());
+  // std::cout << "  pop %ebx" << std::endl; //reg2
+  // std::cout << "  pop %eax" << std::endl; //reg1
+  // std::cout << "  cmp %eax, %ebx" << std::endl;
+  // std::cout << "  je " << equalLabel << std::endl;
+  //might be wrong
 }
 
-void CodeGenerator::visitGreaterNode(GreaterNode* node) {
-    // WRITEME: Replace with code if necessary
+void CodeGenerator::visitAndNode(AndNode* node) { //might be wrong
+  // WRITEME: Replace with code if necessary
+  node->visit_children(this);
+  std::cout << "  # AND" << std::endl;
+  std::cout << "  pop %ebx" << std::endl; //reg2
+  std::cout << "  pop %eax" << std::endl; //reg1
+  std::cout << "  andl %ebx, %eax" << std::endl; //(reg1 && reg2) -> reg1
+  std::cout << "  push %eax" << std::endl; //push reg1
 }
 
-void CodeGenerator::visitGreaterEqualNode(GreaterEqualNode* node) {
-    // WRITEME: Replace with code if necessary
+void CodeGenerator::visitOrNode(OrNode* node) { //might be wrong
+  node->visit_children(this);
+  std::cout << "  # OR" << std::endl;
+  std::cout << "  pop %ebx" << std::endl; //reg2
+  std::cout << "  pop %eax" << std::endl; //reg1
+  std::cout << "  orl %ebx, %eax" << std::endl; //(reg1 && reg2) -> reg1
+  std::cout << "  push %eax" << std::endl; //push reg1
+  // WRITEME: Replace with code if necessary
 }
 
-void CodeGenerator::visitEqualNode(EqualNode* node) {
-    // WRITEME: Replace with code if necessary
-}
-
-void CodeGenerator::visitAndNode(AndNode* node) {
-    // WRITEME: Replace with code if necessary
-}
-
-void CodeGenerator::visitOrNode(OrNode* node) {
-    // WRITEME: Replace with code if necessary
-}
-
-void CodeGenerator::visitNotNode(NotNode* node) {
-    // WRITEME: Replace with code if necessary
+void CodeGenerator::visitNotNode(NotNode* node) { //might be wrong
+  // WRITEME: Replace with code if necessary
+  node->visit_children(this);
+  std::cout << "  # NOT" << std::endl;
+  std::cout << "  pop %eax" << std::endl; //reg1
+  std::cout << "  not %eax" << std::endl; //(!reg1) -> reg1
+  std::cout << "  push %eax" << std::endl; //push reg1
 }
 
 void CodeGenerator::visitNegationNode(NegationNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
+  node->visit_children(this);
+  std::cout << " #NEGATION" << std::endl;
+  std::cout << " pop %eax" << std::endl;
+  std::cout << " neg %eax" << std::endl;
+  std::cout << " push %eax" << std::endl; 
 }
 
 void CodeGenerator::visitMethodCallNode(MethodCallNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
 }
 
 void CodeGenerator::visitMemberAccessNode(MemberAccessNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
 }
 
 void CodeGenerator::visitVariableNode(VariableNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
 }
 
 void CodeGenerator::visitIntegerLiteralNode(IntegerLiteralNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
 }
 
 void CodeGenerator::visitBooleanLiteralNode(BooleanLiteralNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
 }
 
 void CodeGenerator::visitNewNode(NewNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
 }
 
 void CodeGenerator::visitIntegerTypeNode(IntegerTypeNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
 }
 
 void CodeGenerator::visitBooleanTypeNode(BooleanTypeNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
 }
 
 void CodeGenerator::visitObjectTypeNode(ObjectTypeNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
 }
 
 void CodeGenerator::visitNoneNode(NoneNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
 }
 
 void CodeGenerator::visitIdentifierNode(IdentifierNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
 }
 
 void CodeGenerator::visitIntegerNode(IntegerNode* node) {
-    // WRITEME: Replace with code if necessary
+  // WRITEME: Replace with code if necessary
 }
--- project6/parser.y
+++ project6/parser.y
@@ -18,17 +18,206 @@
 /* NOTE: You may use the %glr-parser directive, which may allow your parser to
          work even with some shift/reduce conflicts remianing. */
 
-/* WRITEME: Copy your token and precedence specifiers from Project 5 here. */
 
-/* WRITEME: Copy your type specifiers from Project 5 here. */
 
+/* WRITEME: Copy your token and precedence specifiers from Project 4 here. */
+
+%token INT BOOL FLOAT
+%token ARROW 
+%token IF WHILE DO ELSE PRINT 
+%token PLUS MINUS MULT DIV
+%token GREATER GREATER_THAN_OR_EQUAL
+%token AND OR NOT 
+%token UNARY_MINUS
+%token NAME 
+%token INT_VAL FLOAT_VAL TRUE FALSE
+%token NEW NONE DOT
+%token SEMICOLON COMMA LEFT_BRACE RIGHT_BRACE LEFT_PAREN RIGHT_PAREN
+%token EQUALS ASSIGN
+%token RETURN EXTENDS
+
+ /* WRITEME: Specify precedence here */
+
+%left OR 
+%left AND
+%left GREATER GREATER_THAN_OR_EQUAL EQUALS
+%left PLUS MINUS
+%left MULT DIV
+%right NOT UNARY_MINUS
+%left RETURN
+
+/* WRITEME: Copy your type specifiers from Project 4 here. */
+%type <program_ptr> Program Start  
+%type <class_list_ptr> Classes
+%type <class_ptr> Class
+%type <identifier_ptr> Name 
+%type <none_ptr> NONE
+%type <booleanliteral_ptr> TRUE FALSE
+%type <booleantype_ptr> BOOL
+%type <base_int> INT_VAL
+%type <base_char_ptr> NAME
+%type <integertype_ptr> INT
+%type <declaration_list_ptr> Members Declarations
+%type <memberaccess_ptr> Member_Access
+%type <declaration_ptr> Declaration Member
+%type <expression_list_ptr> Arguments Arguments_Optional
+%type <method_list_ptr> Methods
+%type <method_ptr> Method
+%type <parameter_list_ptr> Parameters
+%type <parameter_ptr> Parameter
+%type <type_ptr> Type
+%type <methodbody_ptr> MethodBody
+%type <statement_list_ptr> Statements
+%type <statement_ptr> Statement
+%type <returnstatement_ptr> Return
+%type <identifier_list_ptr> Names
+%type <expression_ptr> Expression
+%type <assignment_ptr> Assignment
+%type <methodcall_ptr> Method_Call
+%type <ifelse_ptr> if_else 
+%type <while_ptr> while
+%type <dowhile_ptr> do_while
+%type <print_ptr> print
 %%
 
 /* WRITEME: This rule is a placeholder. Replace it with your grammar
-            rules and actions from Project 5. */
+            rules and actions from Project 4. */
 
-Start :           { /* Empty action */ }
-      ;
+Start : Program {$$ = $1; astRoot = $$;}
+; 
+
+Program : Classes {$$ = new ProgramNode($1);}
+;
+
+Classes : Classes Class {$$ = $1; $$->push_back($2);}
+| Class {$$ = new std::list<ClassNode*>(); $$->push_back($1);}
+;
+
+Class : Name EXTENDS Name LEFT_BRACE Members Methods RIGHT_BRACE {$$ = new ClassNode($1, $3, $5, $6);}
+| Name EXTENDS Name LEFT_BRACE  Methods RIGHT_BRACE {$$ = new ClassNode($1, $3, NULL, $5);}
+| Name EXTENDS Name LEFT_BRACE Members RIGHT_BRACE {$$ = new ClassNode($1, $3, $5, NULL);}
+| Name EXTENDS Name LEFT_BRACE RIGHT_BRACE {$$ = new ClassNode($1, $3, NULL, NULL);}
+| Name LEFT_BRACE Members Methods RIGHT_BRACE {$$ = new ClassNode($1, NULL, $3, $4);}
+| Name LEFT_BRACE Members RIGHT_BRACE {$$ = new ClassNode($1, NULL, $3, NULL);}
+| Name LEFT_BRACE Methods RIGHT_BRACE {$$ = new ClassNode($1, NULL, NULL, $3);}
+| Name LEFT_BRACE RIGHT_BRACE {$$ = new ClassNode($1, NULL, NULL, NULL);}
+;
+
+Members : Members Member {$$ = $1; $$->push_back($2);}
+| Member {$$ = new std::list<DeclarationNode*>(); $$->push_back($1);}
+;
+
+Member : Type Names SEMICOLON {$$ = new DeclarationNode($1, $2); } 
+;
+Name : NAME {$$ = new IdentifierNode($1);}
+;
+
+
+Type : INT {$$ = new IntegerTypeNode(); $$->basetype = bt_integer;}
+| BOOL {$$ = new BooleanTypeNode(); $$->basetype = bt_boolean;}
+| Name {$$ = new ObjectTypeNode($1); $$->basetype = bt_object;}
+| NONE {$$ = new NoneNode(); $$->basetype = bt_none;}
+    ;
+
+Methods : Methods Method {$$ = $1; $$->push_back($2);}
+| Method  {$$ = new std::list<MethodNode*>(); $$->push_back($1);}
+;
+
+Method : Name LEFT_PAREN Parameters RIGHT_PAREN ARROW Type MethodBody {$$ = new MethodNode($1, $3, $6, $7);}
+| Name LEFT_PAREN Parameters RIGHT_PAREN MethodBody {$$ = new MethodNode($1, $3, NULL, $5);}
+| Name LEFT_PAREN RIGHT_PAREN ARROW Type MethodBody {$$ = new MethodNode($1, NULL, $5, $6);}
+| Name LEFT_PAREN RIGHT_PAREN MethodBody {$$ = new MethodNode($1, NULL, NULL, $4);}
+;
+
+
+MethodBody : LEFT_BRACE Declarations Statements Return RIGHT_BRACE {$$ = new MethodBodyNode($2, $3, $4);}
+| LEFT_BRACE Declarations Return RIGHT_BRACE {$$ = new MethodBodyNode($2, NULL, $3);}
+| LEFT_BRACE Declarations Statements RIGHT_BRACE {$$ = new MethodBodyNode($2, $3, NULL);}
+| LEFT_BRACE Statements Return RIGHT_BRACE {$$ = new MethodBodyNode(NULL, $2, $3);}
+| LEFT_BRACE Declarations RIGHT_BRACE {$$ = new MethodBodyNode($2, NULL, NULL);}
+| LEFT_BRACE Return RIGHT_BRACE {$$ = new MethodBodyNode(NULL, NULL, $2);}
+| LEFT_BRACE Statements RIGHT_BRACE {$$ = new MethodBodyNode(NULL, $2, NULL);}
+| LEFT_BRACE RIGHT_BRACE {$$ = new MethodBodyNode(NULL, NULL, NULL);}
+;
+
+Parameters : Parameters COMMA Parameter {$$ = $1; $$->push_back($3);}
+| Parameter {$$ = new std::list<ParameterNode*>(); $$->push_back($1); }
+| %empty {$$ = new std::list<ParameterNode*>(); }
+;
+
+Parameter : Type Name {$$ = new ParameterNode($1, $2);}
+; 
+
+Declarations : Declarations Declaration  {$$ = $1; $$->push_back($2);}
+| Declaration {$$ = new std::list<DeclarationNode*>(); $$->push_back($1); }
+;
+
+
+Declaration : Type Names SEMICOLON {$$ = new DeclarationNode($1, $2);}      
+; 
+
+Names :  Names COMMA Name {$$ = $1; $$-> push_back($3);}
+| Name {$$ = new std::list<IdentifierNode*>(); $$->push_back($1); }
+;
+
+Statements : Statements Statement {$$ = $1; $$->push_back($2);}
+| Statement {$$ = new std::list<StatementNode*>(); $$->push_back($1); }
+;
+
+Statement : Name ASSIGN Expression SEMICOLON {$$ = new AssignmentNode($1, NULL, $3);}
+| Name DOT Name ASSIGN Expression SEMICOLON {$$ = new AssignmentNode($1, $3, $5);}     
+| Name ARROW Name ASSIGN Expression SEMICOLON {$$ = new AssignmentNode($1, $3, $5);}                                                                                                                          
+| PRINT Expression SEMICOLON {$$ = new PrintNode($2); }
+| IF Expression LEFT_BRACE Statements RIGHT_BRACE {$$ = new IfElseNode($2,$4, NULL); }
+| IF Expression LEFT_BRACE Statements RIGHT_BRACE ELSE LEFT_BRACE Statements RIGHT_BRACE {$$ = new IfElseNode($2,$4, $8); }
+| WHILE Expression LEFT_BRACE Statements RIGHT_BRACE {$$ = new WhileNode($2,$4); } 
+| DO LEFT_BRACE Statements RIGHT_BRACE WHILE LEFT_PAREN Expression RIGHT_PAREN SEMICOLON {$$ = new DoWhileNode($3,$7); }
+| Name DOT Name LEFT_PAREN Arguments RIGHT_PAREN SEMICOLON {$$ = new CallNode(new MethodCallNode($1, $3, $5)); } 
+| Name ARROW Name LEFT_PAREN Arguments RIGHT_PAREN SEMICOLON {$$ = new CallNode(new MethodCallNode($1, $3, $5)); } 
+| Name LEFT_PAREN Arguments RIGHT_PAREN SEMICOLON {$$ = new CallNode(new MethodCallNode($1, NULL, $3)); }
+| Name ARROW Name LEFT_PAREN RIGHT_PAREN SEMICOLON {$$ = new CallNode(new MethodCallNode($1, $3, NULL)); } 
+| Name DOT Name LEFT_PAREN RIGHT_PAREN SEMICOLON {$$ = new CallNode(new MethodCallNode($1, $3, NULL)); } 
+| Name LEFT_PAREN RIGHT_PAREN SEMICOLON {$$ = new CallNode(new MethodCallNode($1, NULL, NULL)); }  
+;
+
+Expression : INT_VAL {$$ = new IntegerLiteralNode(new IntegerNode($1)); }
+| Expression PLUS Expression {$$ = new PlusNode($1, $3); }
+| Expression MINUS Expression {$$ = new MinusNode($1, $3); }
+| Expression MULT Expression {$$ = new TimesNode($1, $3); }
+| Expression DIV Expression {$$ = new DivideNode($1, $3); }
+| Expression GREATER Expression {$$ = new GreaterNode($1, $3); }
+| Expression GREATER_THAN_OR_EQUAL Expression {$$ = new GreaterEqualNode($1, $3); }
+| Expression EQUALS Expression {$$ = new EqualNode($1, $3); }
+| Expression AND Expression  {$$ = new AndNode($1, $3); }
+| Expression OR Expression {$$ = new OrNode($1, $3); }
+| NOT Expression {$$ = new NotNode($2); }
+| MINUS Expression %prec UNARY_MINUS {$$ = new NegationNode($2); }
+| Name {$$ = new VariableNode($1); }
+| Name LEFT_PAREN Arguments RIGHT_PAREN  {$$ = new MethodCallNode($1, NULL, $3); }
+| Name DOT Name LEFT_PAREN Arguments RIGHT_PAREN {$$ = new MethodCallNode($1, $3, $5); }  
+| Name ARROW Name LEFT_PAREN Arguments RIGHT_PAREN {$$ = new MethodCallNode($1, $3, $5); } 
+| Name LEFT_PAREN RIGHT_PAREN  {$$ = new MethodCallNode($1, NULL, NULL); }
+| Name DOT Name LEFT_PAREN RIGHT_PAREN {$$ = new MethodCallNode($1, $3, NULL); }  
+| Name ARROW Name LEFT_PAREN RIGHT_PAREN {$$ = new MethodCallNode($1, $3, NULL); }                                                                                                                    
+| Name DOT Name {$$ = new MemberAccessNode($1, $3);}
+| Name ARROW Name {$$ = new MemberAccessNode($1, $3);}                                                                                                                                                        
+| LEFT_PAREN Expression RIGHT_PAREN {$$ = $2;}
+| TRUE {$$ = new BooleanLiteralNode(new IntegerNode(1)); }
+| FALSE {$$ = new BooleanLiteralNode(new IntegerNode(0)); }
+| NEW Name {$$ = new NewNode($2,NULL); } 
+| NEW Name LEFT_PAREN Arguments_Optional RIGHT_PAREN {$$ = new NewNode($2,$4); }  
+; 
+
+Arguments_Optional : Arguments  {$$ = $1;}
+| %empty {$$ = new std::list<ExpressionNode*>();}
+;
+ 
+Arguments : Arguments COMMA Expression {$$ = $1; $$->push_back($3);}
+| Expression  {$$ = new std::list<ExpressionNode*>(); $$->push_back($1);}
+;
+			
+Return : RETURN Expression SEMICOLON {$$ = new ReturnStatementNode($2); }
+;
 
 %%
 
